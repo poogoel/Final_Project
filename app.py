32d578592ed74ac49eb9cc4447c33c43c4ede264
rjones17 <jones.rebekah17@gmail.com>
2020-01-15 17:40:08 -0600

    updates to app.py file

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template
+# import necessary libraries
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+
+app = Flask(__name__)
+
 from flask_sqlalchemy import SQLAlchemy
-import sys
-import json
+from sqlalchemy import create_engine
+engine = create_engine('postgres+psycopg2://postgres:changeme@localhost:5432/team_stats')
 
-app = Flask( __name__ )
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql:arn:aws:rds:us-east-2:881778463513:db:dataviz"
 db = SQLAlchemy(app)
 
+from models import Stats
+
+# create route that renders index.html template
 @app.route("/")
-def index():
-    """Return the homepage."""
+def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
+
