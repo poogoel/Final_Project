d562d7e4be3315d94a5c44fcf69cf71db3600716
rjones17 <jones.rebekah17@gmail.com>
2020-01-16 10:48:05 -0600

    reordered Flask app

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -6,24 +6,29 @@ from flask import (
     jsonify,
     request,
     redirect)
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Stats
 
 app = Flask(__name__)
 
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-engine = create_engine('postgres+psycopg2://postgres:changeme@localhost:5432/team_stats')
-
-
+DATABASE_URI = 'postgres+psycopg2://postgres:changeme@localhost:5432/team_stats'
+engine = create_engine(DATABASE_URI)
 db = SQLAlchemy(app)
 
-from models import Stats
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # create route that renders index.html template
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/alldata")
+def alldata():
+    alldata = db.session.query(Stats).all()
+    return jsonify(alldata)
+
 if __name__ == "__main__":
     app.run()
-
